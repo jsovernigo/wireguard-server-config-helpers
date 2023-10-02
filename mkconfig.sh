4155813b021a7ae2926ced5f20badca9561b84b8
Julian Sovernigo <juliansovernigo@gmail.com>
2023-10-02 13:52:38 -0400

    Added files.

--- mkconfig.sh
+++ mkconfig.sh
@@ -1,58 +1,53 @@
 #!/usr/bin/bash -e
 
-function usage {
-  echo "${0} <name> <ip>"
+set -x
+
+function usage () {
+  echo "${0} <wginterface> <name> <ip>"
 }
 
-function create_client_config {
-  client_config_file="${1}.conf"
+function create_client_config () {
   allowed_ips="${2}"
   echo "Creating client config ${client_config_file} with allowed ips: ${allowed_ips}"
-  # TODO make server public key configurable as command line arg
-  cat > "${client_config_file}" <<EOF
-[Interface]
-Address = ${ip}/32
-PrivateKey = ${private}
-DNS = 1.1.1.1
-
-[Peer]
-PublicKey = GwWj+BkDxhTmCwXQ0diW2yMpoet2xMzH9RuEegp/QGA=
-Endpoint = orion.hugo-klepsch.tech:51820
-AllowedIPs = ${allowed_ips}
-EOF
+  envsubst < 'templates/client.conf.template' > "${client_config_file}"
 }
 
-name="${1}"
-ip="${2}"
-interface="wg0"  # TODO make configurable
+# set variables for the script
+wginterface="${1}"
+name="${2}"
+ip="${3}"
 
-if [[ -z "${name}" ]]; then
+client_config_file="${name}.conf"
+server_config_file="server.conf"
+server_public=$(cat publickey)
+
+# ensure cli arguments exist
+if [[ -z "${name}" ]] || [[ -z "${ip}" ]] || [[-z "${wginterface}"]]; then
   usage
   exit 1
 fi
-if [[ -z "${ip}" ]]; then
-  usage
-  exit 1
-fi
 
+# move to client directory.
 mkdir "${name}"
 pushd "${name}"
 
 echo "Creating keys"
+
+# set mask, save old mask
 o_umask=$(umask)
 echo "old umask is ${o_umask}"
 umask 077
-wg genkey | tee private | wg pubkey > public
 
+# create client keys and generate config
+wg genkey | tee private | wg pubkey > public
 private=$(<private)
 public=$(<public)
 
-create_client_config "${name}all" "0.0.0.0/0, ::/0"
-create_client_config "${name}" "10.8.0.1/24"
+create_client_config "${name}" "${serveraddr}"
 
+# reset old mask
 umask "${o_umask}"
 
-server_config_file="server.conf"
 echo "Creating server config ${server_config_file}"
 cat > "${server_config_file}" <<EOF
 [Peer]
@@ -60,7 +55,8 @@ PublicKey = ${public}
 AllowedIPs = ${ip}/32
 EOF
 
-echo "Adding server config to interface ${interface}"
-sudo wg addconf "${interface}" "${server_config_file}"
+# add new confg file
+echo "Adding server config to interface ${wginterface}"
+sudo wg addconf "${wginterface}" "${server_config_file}"
 
 popd
